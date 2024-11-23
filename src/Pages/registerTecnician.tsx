56384949814e7f97b3eeb3e48520624674e07d11
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-23 14:58:16 -0300

    feat: backend integration

--- src/Pages/registerTecnician.tsx
+++ src/Pages/registerTecnician.tsx
@@ -3,8 +3,11 @@ import technicianSchema, {
   registerTechnicianType,
 } from "../schemas/registerTechnician";
 import { zodResolver } from "@hookform/resolvers/zod";
+import useRegister from "../hook/useRegister";
 
 const RegisterTechnician = () => {
+  const { registerTechnician } = useRegister();
+
   const {
     register,
     handleSubmit,
@@ -13,17 +16,19 @@ const RegisterTechnician = () => {
     resolver: zodResolver(technicianSchema),
   });
 
-  const onSubmit: SubmitHandler<any> = (data) => console.log(data);
-
+  const onSubmit: SubmitHandler<registerTechnicianType> = (data) =>
+    registerTechnician(data);
   return (
     <div>
       <form>
-        <input type="text" {...register("name")} />
+        <input type="text" {...register("name")} placeholder="Nome" />
         {errors.name && <p>{errors.name.message}</p>}
-        <input type="text" {...register("email")} />
+        <input type="text" {...register("email")} placeholder="Email" />
         {errors.email && <p>{errors.email.message}</p>}
-        <input type="password" {...register("password")} />
+        <input type="password" {...register("password")} placeholder="Senha" />
         {errors.password && <p>{errors.password.message}</p>}
+        <input type="text" {...register("phone")} placeholder="Telefone" />
+        {errors.phone && <p>{errors.phone.message}</p>}
       </form>
       <button onClick={handleSubmit(onSubmit)}>Criar</button>
     </div>
