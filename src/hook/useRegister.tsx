421e08f94693589aeb635cb28d28ea40c0d8edfd
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-23 16:06:10 -0300

    feat: use hook useNotify to give feedback to the user

--- src/hook/useRegister.tsx
+++ src/hook/useRegister.tsx
@@ -1,19 +1,32 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { BASE_URL } from "../constants";
 import { registerTechnicianType } from "../schemas/registerTechnician";
 import { useNavigate } from "react-router-dom";
+import useNotify from "./useNotify";
+import { notifyPositionMap, notifyType } from "../types";
 
 const useRegister = () => {
   const navigate = useNavigate();
+  const notify = useNotify();
 
   const registerTechnician = async (data: registerTechnicianType) => {
     const urlRegisterTechnician = `${BASE_URL}/register/technician`;
 
     try {
       await axios.post(urlRegisterTechnician, data);
+      notify(
+        "Técnico Registrado com Sucesso!",
+        notifyPositionMap.topRight,
+        notifyType.success
+      );
       navigate("/login");
     } catch (error) {
-      console.error(error);
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
     }
   };
 
