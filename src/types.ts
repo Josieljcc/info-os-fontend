8c1bbe1bde42276e7fb020e5c0475548cf35883b
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-23 16:04:39 -0300

    feat: create useNotify hook

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export enum notifyType {
+  success = "success",
+  error = "error",
+  warn = "warn",
+  info = "info",
+}
+
+export enum notifyPositionMap {
+  topCenter = "top-center",
+  topLeft = "top-left",
+  topRight = "top-right",
+  bottomLeft = "bottom-left",
+  bottomRight = "bottom-right",
+  bottomCenter = "bottom-center",
+}
