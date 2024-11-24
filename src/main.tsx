1edda4f95bca16c458b6ac39bbecca7f84ce7fa2
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-24 19:00:32 -0300

    style: reorder imports for improved code readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./Routes/router.tsx";
 import { ToastContainer } from "react-toastify";
+import router from "./Routes/router.tsx";
+import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
