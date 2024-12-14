695da76c6bf972c3e8e44acfab791c28cbb36b77
cwesly <cauawesly@outlook.com>
2024-12-14 15:38:28 -0300

    feat: create register client form and logic validation.

--- src/Pages/registerClient.tsx
+++ src/Pages/registerClient.tsx
@@ -2,10 +2,29 @@ import { useContext, useEffect } from "react";
 import UserContext from "../context/userContext";
 import { role } from "../types";
 import { useNavigate } from "react-router-dom";
+import Input from "../components/input/input";
+import { zodResolver } from "@hookform/resolvers/zod/src/zod.js";
+import clientSchema, { registerClientType } from "../schemas/registerClient";
+import { FormProvider, useForm } from "react-hook-form";
+import ButtonPrimary from "../components/buttonPrimary/buttonPrimary";
 
 const RegisterClient = () => {
   const { user, setUser } = useContext(UserContext);
+
   const navigate = useNavigate();
+
+  const methods = useForm<registerClientType>({
+    resolver: zodResolver(clientSchema),
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const handleCreateClient = (data: registerClientType) => {
+    console.log(data);
+  };
 
   useEffect(() => {
     const localStorageUser =
@@ -22,7 +41,35 @@ const RegisterClient = () => {
     setUser(localStorageUser);
   }, []);
 
-  return <p>Hello</p>;
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <Input formName="name" placeholder="Nome" />
+        {errors.name && (
+          <p className="text-red-500 text-sm">{errors.name.message}</p>
+        )}
+        <Input formName="email" placeholder="Email" />
+        {errors.email && (
+          <p className="text-red-500 text-sm">{errors.email.message}</p>
+        )}
+        <Input formName="password" placeholder="Senha" type="password" />
+        {errors.password && (
+          <p className="text-red-500 text-sm">{errors.password.message}</p>
+        )}
+        <Input formName="phone" placeholder="Telefone" />
+        {errors.phone && (
+          <p className="text-red-500 text-sm">{errors.phone.message}</p>
+        )}
+        <Input formName="address" placeholder="Endereço" />
+        {errors.address && (
+          <p className="text-red-500 text-sm">{errors.address.message}</p>
+        )}
+        <ButtonPrimary onClick={handleSubmit(handleCreateClient)}>
+          Enviar
+        </ButtonPrimary>
+      </FormProvider>
+    </div>
+  );
 };
 
 export default RegisterClient;
