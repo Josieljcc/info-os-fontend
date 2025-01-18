422df9578fd191c2c6127df6015677a25f87c444
Josuejcs13 <josuejcs13@gmail.com>
2025-01-18 14:45:56 -0500

    fix: drawer style

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -41,14 +41,11 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-3/4 flex-col rounded-t-[10px] bg-[#27262C] ",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 pt-11 flex flex-col rounded-t-[10px] bg-[#27262C] ",
         className
       )}
       {...props}
     >
-      <div className="relative">
-        <div className="mx-auto  mt-4 h-2 w-[100px] rounded-full bg-muted absolute -top-24 inset-0 m-auto" />
-      </div>
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
