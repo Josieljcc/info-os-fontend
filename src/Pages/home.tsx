718e9ba3a9c7c133a64a57ed0f53a89f196cde6f
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-02-15 16:00:07 -0300

    refactor: add use hook authentication and create link to list technicians

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -2,30 +2,12 @@ import FormClient from "@/components/formClient/formClient";
 import FormDrawer from "@/components/formDrawer/formDrawer";
 import FormPart from "@/components/formPart/formPart";
 import FormService from "@/components/formService/formService";
-
-import UserContext from "@/context/userContext";
-import { role } from "@/types";
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import useAuthentication from "@/hook/useAuthentication";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const { user, setUser } = useContext(UserContext);
-  const navigate = useNavigate();
+  useAuthentication();
 
-  useEffect(() => {
-    const localStorageUser =
-      JSON.parse(localStorage.getItem("user") as string) || {};
-
-    if (
-      user.role === role.client ||
-      localStorageUser?.role === role.client ||
-      (!user.token && !localStorageUser.token)
-    ) {
-      navigate("/login");
-    }
-
-    setUser(localStorageUser);
-  }, []);
   return (
     <div className="h-screen flex flex-col w-full bg-[#141414] ">
       <h1>HOME</h1>
@@ -38,6 +20,7 @@ const Home = () => {
       <FormDrawer buttonTitle="Service" title="Cadastrar Serviço">
         <FormService />
       </FormDrawer>
+      <Link to={"/technician"}>Listar Técnicos</Link>
     </div>
   );
 };
