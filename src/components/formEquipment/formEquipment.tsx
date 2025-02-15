56256733b8a7e61e1629e11f07be16b0f8488594
Josuejcs13 <josuejcs13@gmail.com>
2025-02-15 12:10:40 -0500

    feat: create formEquipment and integration with backEnd

--- src/components/formEquipment/formEquipment.tsx
+++ src/components/formEquipment/formEquipment.tsx
@@ -3,14 +3,35 @@ import Input from "../input/input";
 import equipmentSchema, { EquipmentType } from "@/schemas/equipment";
 import { zodResolver } from "@hookform/resolvers/zod";
 import ButtonPrimary from "../buttonPrimary/buttonPrimary";
-import useRegister from "@/hook/useRegister";
+import InputSelect from "../clientSelect/clientSelect";
+import useClient from "@/hook/useClient";
+import { useEffect, useState } from "react";
+import { Client } from "@/types";
+
+import DrawerClient from "../drawerClient/drawerClient";
+import useEquipment from "@/hook/useEquipment";
 
 const FormEquipment = () => {
-  const { registerEquipment } = useRegister();
+  const [clients, setClients] = useState<Client[]>([]);
+
+  const [clientID, setClientID] = useState<number>(0);
+
+  const { registerEquipment } = useEquipment();
 
   const methods = useForm<EquipmentType>({
     resolver: zodResolver(equipmentSchema),
   });
+
+  const { getAllClients } = useClient();
+
+  useEffect(() => {
+    const getClients = async () => {
+      const data = await getAllClients();
+      setClients(data as Client[]);
+    };
+
+    getClients();
+  }, []);
 
   const {
     handleSubmit,
@@ -18,35 +39,54 @@ const FormEquipment = () => {
   } = methods;
 
   const handleCreateEquipment = (data: EquipmentType) => {
-    registerEquipment(data);
+    registerEquipment(data, clientID);
   };
-
   return (
-    <div>
+    <div className="h-full flex flex-col gap-4 font-medium  w-3/4 md:w-1/2">
       <FormProvider {...methods}>
-        <Input formName="name" placeholder="Nome" className="" />
+        <Input
+          formName="name"
+          placeholder="Nome"
+          className="py-8 text-xl placeholder-[#D4D4D8] text-[#D4D4D8]"
+        />
         {errors.name && (
           <p className="text-red-500 text-sm">{errors.name.message}</p>
         )}
-        <Input formName="description" placeholder="Descrição" className="" />
+        <Input
+          formName="description"
+          placeholder="Descrição"
+          className="py-8 text-xl placeholder-[#D4D4D8] text-[#D4D4D8]"
+        />
         {errors.description && (
           <p className="text-red-500 text-sm">{errors.description.message}</p>
         )}
         <Input
           formName="serialNumber"
           placeholder="Numero de Serie"
-          className=""
+          className="py-8 text-xl placeholder-[#D4D4D8] text-[#D4D4D8]"
         />
         {errors.serialNumber && (
           <p className="text-red-500 text-sm">{errors.serialNumber.message}</p>
         )}
-        <Input formName="equipmentModel" placeholder="Modelo" className="" />
-        {errors.equipmentModel && (
-          <p className="text-red-500 text-sm">
-            {errors.equipmentModel.message}
-          </p>
+        <Input
+          formName="model"
+          placeholder="Modelo"
+          className="py-8 text-xl placeholder-[#D4D4D8] text-[#D4D4D8]"
+        />
+        {errors.model && (
+          <p className="text-red-500 text-sm">{errors.model.message}</p>
         )}
       </FormProvider>
+      {!clients ? (
+        <DrawerClient />
+      ) : (
+        <InputSelect
+          placeholder="Selecione Cliente"
+          clients={clients}
+          setClientID={setClientID}
+        />
+      )}
+
       <div className="flex justify-center ">
         <ButtonPrimary
           onClick={handleSubmit(handleCreateEquipment)}
--- src/schemas/equipment.ts
+++ src/schemas/equipment.ts
@@ -6,7 +6,7 @@ const equipmentSchema = z.object({
   name: z.string().min(1, "Nome do equipamento é obrigatório"),
   description: z.string(),
   serialNumber: z.string(),
-  equipmentModel: z.string(),
+  model: z.string(),
 });
 
 export default equipmentSchema
