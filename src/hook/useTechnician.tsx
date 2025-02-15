f8ec396909988d01fd0b19b83be12be944d9c0cf
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-02-15 15:58:40 -0300

    feat: create useTechnician hook

---
+++ src/hook/useTechnician.tsx
@@ -0,0 +1,64 @@
+import { BASE_URL } from "@/constants";
+import { registerTechnicianType } from "@/schemas/registerTechnician";
+import { notifyPositionMap, notifyType, Technician } from "@/types";
+import axios, { AxiosError } from "axios";
+import useNotify from "./useNotify";
+import { useNavigate } from "react-router-dom";
+import UserContext from "@/context/userContext";
+import { useContext, useState } from "react";
+
+const useTechnician = () => {
+  const navigate = useNavigate();
+  const notify = useNotify();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const {
+    user: { token },
+  } = useContext(UserContext);
+
+  const header = { headers: { Authorization: `Bearer ${token}` } };
+  console.log("token", token);
+
+  const getAllTechnician = async () => {
+    const urlTechnician = `${BASE_URL}/technician`;
+
+    try {
+      const response = await axios.get(urlTechnician, header);
+      setIsLoading(false);
+      return response.data as Technician[];
+    } catch (error) {
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  const registerTechnician = async (data: registerTechnicianType) => {
+    const urlRegisterTechnician = `${BASE_URL}/register/technician`;
+
+    try {
+      await axios.post(urlRegisterTechnician, data);
+
+      notify(
+        "Técnico Registrado com Sucesso!",
+        notifyPositionMap.topRight,
+        notifyType.success
+      );
+      navigate("/login");
+    } catch (error) {
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  return { registerTechnician, getAllTechnician, isLoading };
+};
+
+export default useTechnician;
