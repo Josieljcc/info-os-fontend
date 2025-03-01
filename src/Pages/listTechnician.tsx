8945cb0f9432092e1411fbaa508c5e7d2430024a
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-03-01 13:57:24 -0300

    fix: center loading spinner on all screen sizes

--- src/Pages/listTechnician.tsx
+++ src/Pages/listTechnician.tsx
@@ -21,7 +21,7 @@ const ListTechnician = () => {
 
   if (isLoading) {
     return (
-      <div className="h-screen bg-main-bg bg-cover overflow-hidden bg-center flex flex-col justify-center pt-24 px-8 pb-5 md:items-center">
+      <div className="h-screen  bg-main-bg bg-cover overflow-hidden bg-center flex flex-col justify-center pt-24 px-8 pb-5 items-center">
         <div className="loader animate-spin" />
       </div>
     );
