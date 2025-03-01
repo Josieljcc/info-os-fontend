9ce757204c3ee490fed9ae7fbfb965e9d26a8453
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-03-01 17:00:46 -0300

    refactor: change technicianCard to generic component Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,26 @@
-import { Technician } from "@/types";
+import { Client, Technician } from "@/types";
 import ButtonPrimary from "../buttonPrimary/buttonPrimary";
 import { GrFormEdit } from "react-icons/gr";
 import { RiDeleteBin7Line } from "react-icons/ri";
 import { RxAvatar } from "react-icons/rx";
 
-type TechnicianCardProps = {
-  technician: Technician;
+type CardProps = {
+  item: Technician | Client;
 };
 
-const TechnicianCard = ({ technician }: TechnicianCardProps) => {
+const Card = ({ item }: CardProps) => {
   return (
-    <div className="flex justify-between bg-[#3F3F46] text-white rounded-lg shadow-md border border-gray-600 md:flex-col">
+    <div className="flex justify-between bg-[#3F3F46] text-white rounded-lg shadow-md border border-gray-600 md:flex-col md:min-w-32">
       <div className="hidden md:flex md:justify-center md:items-center md:text-6xl md:mt-2">
         <RxAvatar />
       </div>
       <div className="p-2 md:flex md:gap-3">
         <div>
           <h3 className="text-lg font-semibold w-48 md:w-28 lg:w-full truncate">
-            {technician.name}
+            {item.name}
           </h3>
-          <p className="text-sm md:w-32 lg:w-full truncate">
-            {technician.email}
-          </p>
-          <p className="text-sm md:w-32 lg:w-full truncate">
-            {technician.phone}
-          </p>
+          <p className="text-sm md:w-32 lg:w-40 truncate">{item.email}</p>
+          <p className="text-sm md:w-32 lg:w-40 truncate">{item.phone}</p>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center md:flex-row">
@@ -45,4 +41,4 @@ const TechnicianCard = ({ technician }: TechnicianCardProps) => {
   );
 };
 
-export default TechnicianCard;
+export default Card;
