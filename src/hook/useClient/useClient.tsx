521b4b5196860f82b96bcbda5406480ef15ce8e0
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-04-05 16:51:39 -0300

    feat: add the function to search for customer by name, email and phone

---
+++ src/hook/useClient/useClient.tsx
@@ -0,0 +1,148 @@
+import { BASE_URL } from "@/constants";
+import { registerClientType } from "@/schemas/registerClient";
+import { Client, notifyPositionMap, notifyType } from "@/types";
+import axios, { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
+import useNotify from "../useNotify";
+import UserContext from "@/context/userContext";
+import { useContext, useState } from "react";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { SearchTerm } from "./types";
+
+type ClientPaginatedResponse = {
+  clients: Client[];
+  totalPages: number;
+  page: number;
+};
+
+const PAGESIZE = 10;
+
+const useClient = () => {
+  const navigate = useNavigate();
+  const notify = useNotify();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const {
+    user: { token },
+  } = useContext(UserContext);
+
+  const header = { headers: { Authorization: `Bearer ${token}` } };
+
+  const registerClient = async (data: registerClientType) => {
+    const urlRegisterClient = `${BASE_URL}/client`;
+
+    try {
+      await axios.post(urlRegisterClient, data, header);
+
+      notify(
+        "Cliente Registrado com Sucesso!",
+        notifyPositionMap.topRight,
+        notifyType.success
+      );
+
+      navigate("/home");
+    } catch (error) {
+      const err = error as AxiosError;
+
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  const getAllClients = async ({
+    pageParam,
+  }: {
+    pageParam: number;
+  }): Promise<ClientPaginatedResponse | undefined> => {
+    const urlClient = `${BASE_URL}/client?page=${pageParam}&pageSize=${PAGESIZE}`;
+
+    try {
+      const response = await axios.get(urlClient, header);
+      setIsLoading(false);
+      return response.data;
+    } catch (error) {
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  const getClientBySearch = async (
+    searchTerm: SearchTerm
+  ): Promise<Client[] | undefined> => {
+    const params = new URLSearchParams();
+
+    if (searchTerm.name) {
+      params.append("name", searchTerm.name);
+    }
+    if (searchTerm.email) {
+      params.append("email", searchTerm.email);
+    }
+    if (searchTerm.phone) {
+      params.append("phone", searchTerm.phone.toString());
+    }
+
+    const urlClient = `${BASE_URL}/client?${params.toString()}`;
+
+    try {
+      const response = await axios.get(urlClient, header);
+      return response.data.clients;
+    } catch (error) {
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  const {
+    data: paginatedClients,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["getAllClients"],
+    queryFn: getAllClients,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.totalPages === lastPage?.page) return;
+      return Number(lastPage?.page) + 1;
+    },
+  });
+
+  const clients = paginatedClients?.pages.flatMap((page) => {
+    return page?.clients;
+  });
+
+  const getClientById = async (id: number) => {
+    const urlClientById = `${BASE_URL}/client/${id}`;
+
+    const response = await axios.get(urlClientById, header);
+    return response.data;
+  };
+
+  const { data: client } = useQuery({
+    queryKey: ["getClient"],
+    queryFn: () => getClientById(1),
+  });
+
+  return {
+    registerClient,
+    getAllClients,
+    isLoading,
+    client,
+    clients,
+    fetchNextPage,
+    hasNextPage,
+    getClientBySearch,
+  };
+};
+
+export default useClient;
