488bba79326f5dd0f07b0a297a6a3ad30f32e812
Josuejcs13 <josuejcs13@gmail.com>
2025-05-04 00:31:13 -0400

    feat: create ResizeObserver hook for observe and get the bounding client rect of a DOM element.

---
+++ src/hook/useResizeObserver.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
+type ObserverRect = Omit<DOMRectReadOnly, "toJSON">;
+
+const useResizeObserver = () => {
+  const ref = useRef<any>(null);
+  const [rect, setRect] = useState<ObserverRect>();
+
+  useEffect(() => {
+      if (!ref.current) {
+        return;
+      }
+  
+      const observer = new ResizeObserver(() => {
+        ref.current;
+        const boundingRect = ref.current.getBoundingClientRect();
+  
+        setRect(boundingRect);
+      });
+      ref.current;
+      observer.observe(ref.current);
+  
+      return () => observer.disconnect();
+    }, [ref]);
+
+  return { ref, rect };
+};
+export default useResizeObserver;
