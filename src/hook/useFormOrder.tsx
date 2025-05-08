1d47567bc1cd0da22bd8379b83beecd22c8e5f40
Josieljcc <josiel.jcc@hotmail.com>
2025-05-08 18:00:32 -0300

    refactor: remove client from useFormOrder

--- src/hook/useFormOrder.tsx
+++ src/hook/useFormOrder.tsx
@@ -1,7 +1,6 @@
 import UserContext from "@/context/userContext";
-import { Service, Part, Client } from "@/types";
+import { Service, Part } from "@/types";
 import { useState, useContext, useEffect } from "react";
-import useClient from "./useClient/useClient";
 import useOrder, { OrderPayload } from "./useOrder";
 import usePart from "./usePart";
 import UseService from "./useService";
@@ -10,16 +9,12 @@ import { OrderType } from "@/schemas/order";
 const useFormOrder = () => {
   const [services, setServices] = useState<Service[]>([]);
   const [parts, setParts] = useState<Part[]>([]);
-  const [clients, setClients] = useState<Client[]>([]);
   const [selectedServiceId, setSelectedServiceId] = useState<number>();
   const [selectedPartId, setSelectedPartId] = useState<number>();
-  const [clientId, setClientId] = useState<number>();
 
   const { getAllServices } = UseService();
 
   const { getAllPart } = usePart();
-
-  const { getAllClients } = useClient();
 
   const { registerOrder } = useOrder();
 
@@ -43,14 +38,6 @@ const useFormOrder = () => {
     getParts();
   }, []);
 
-  useEffect(() => {
-    const getClients = async () => {
-      const data = await getAllClients();
-      setClients(data as Client[]);
-      console.log(clients);
-    };
-    getClients();
-  }, []);
 
   const handleCreateOrder = (data: OrderType) => {
     const selectedService = services?.find(
@@ -62,17 +49,14 @@ const useFormOrder = () => {
       services: [selectedService as Service],
       parts: [selectedPart as Part],
       technicianId: String(technicianId),
-      clientId: String(clientId),
     };
     registerOrder(payLoad);
   };
 
   return {
-    clients,
     parts,
     services,
     handleCreateOrder,
-    setClientId,
     setSelectedServiceId,
     setSelectedPartId,
   };
