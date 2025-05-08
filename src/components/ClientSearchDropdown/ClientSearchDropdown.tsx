0331d49885b54ba5d1d9207cff0179e27acdf17f
Josieljcc <josiel.jcc@hotmail.com>
2025-05-08 18:05:41 -0300

    feat: create ClientSearchDropdown component

---
+++ src/components/ClientSearchDropdown/ClientSearchDropdown.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import useDebounce from '@/hook/useDebounce/useDebounce';
+import { useClientSearch } from '@/hook/useClient/useSearchClient';
+import { Client } from '@/types';
+import { useFormContext } from 'react-hook-form';
+import DrawerClient from '../drawerClient/drawerClient';
+
+export function ClientSearchDropdown() {
+    const [search, setSearch] = useState("");
+    const [debouncedSearch] = useDebounce(search, 400);
+    const [open, setOpen] = useState(false);
+    const { setValue } = useFormContext();
+
+    const { data: clients } = useClientSearch(
+        { searchTerm: { name: debouncedSearch } }
+    );
+
+    const handleSelectClient = (client: Client) => {
+        if (!client) {
+            return
+        }
+
+        setSearch(client.name);
+        setValue("clientId", String(client.id));
+        setOpen(false);
+    }
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <input
+                    type="text"
+                    className="h-10 w-full pl-8 shadow-md rounded-lg border-2 border-transparent
+                     bg-[#52525B] p-2 hover:border-blue-500 focus:border-blue-500 focus:outline-none py-8 text-xl placeholder-[#D4D4D8] text-[#D4D4D8]"
+                    placeholder="Buscar cliente..."
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                    onFocus={() => setOpen(true)}
+                    onBlur={() => setOpen(false)}
+                />
+            </PopoverTrigger>
+            <PopoverContent onClick={(event) => event.stopPropagation()} className="w-full bg-[#52525B] text-[#D4D4D8]">
+
+                <ul className="space-y-2">
+                    {!clients?.length
+                        ?
+                        <>
+                            <p className="text-zinc-200 text-center">Nenhum cliente encontrado</p>
+                            <DrawerClient />
+                        </>
+                        :
+                        clients?.map((client) => (
+                            <li
+                                key={client.id}
+                                className="cursor-pointer hover:bg-accent hover:text-accent-foreground p-2 rounded"
+                                onClick={() => {
+                                    handleSelectClient(client);
+                                    setSearch(client.name);
+                                }}
+                            >
+                                {client.name}
+                            </li>
+                        ))}
+                </ul>
+
+            </PopoverContent>
+        </Popover>
+    );
+}
