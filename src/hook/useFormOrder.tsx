074b566896b4899022def45ca857f1981d6f817d
Josuejcs13 <josuejcs13@gmail.com>
2025-05-10 13:16:52 -0400

    feat: create list of Os using useInfiniteQuery

--- src/hook/useFormOrder.tsx
+++ src/hook/useFormOrder.tsx
@@ -1,25 +1,25 @@
 import UserContext from "@/context/userContext";
-import { Service, Part, Client } from "@/types";
+import { Service, Part, Order } from "@/types";
 import { useState, useContext, useEffect } from "react";
-import useClient from "./useClient/useClient";
-import useOrder, { OrderPayload } from "./useOrder";
+import useOrder from "./useOrder";
 import usePart from "./usePart";
 import UseService from "./useService";
 import { OrderType } from "@/schemas/order";
+import useClient from "./useClient/useClient";
 
 const useFormOrder = () => {
   const [services, setServices] = useState<Service[]>([]);
   const [parts, setParts] = useState<Part[]>([]);
-  const [clients, setClients] = useState<Client[]>([]);
+
   const [selectedServiceId, setSelectedServiceId] = useState<number>();
   const [selectedPartId, setSelectedPartId] = useState<number>();
   const [clientId, setClientId] = useState<number>();
 
+  const {clients} = useClient()
+
   const { getAllServices } = UseService();
 
   const { getAllPart } = usePart();
-
-  const { getAllClients } = useClient();
 
   const { registerOrder } = useOrder();
 
@@ -43,21 +43,12 @@ const useFormOrder = () => {
     getParts();
   }, []);
 
-  useEffect(() => {
-    const getClients = async () => {
-      const data = await getAllClients();
-      setClients(data as Client[]);
-      console.log(clients);
-    };
-    getClients();
-  }, []);
-
   const handleCreateOrder = (data: OrderType) => {
     const selectedService = services?.find(
       (service) => service.id === selectedServiceId
     );
     const selectedPart = parts?.find((part) => part.id === selectedPartId);
-    const payLoad: OrderPayload = {
+    const payLoad: Order = {
       ...data,
       services: [selectedService as Service],
       parts: [selectedPart as Part],
--- src/hook/useOrder.tsx
+++ src/hook/useOrder.tsx
@@ -1,18 +1,19 @@
 import { BASE_URL } from "@/constants";
 import UserContext from "@/context/userContext";
-import { notifyPositionMap, notifyType, Part, Service } from "@/types";
+import { notifyPositionMap, notifyType, Order, Os } from "@/types";
 import axios, { AxiosError } from "axios";
 import { useContext } from "react";
 import useNotify from "./useNotify";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export type OrderPayload = {
-  date: string;
-  status: string;
-  comment: string;
-  clientId: string;
-  technicianId: string;
-  services: Service[];
-  parts: Part[];
+export type PageParam = {
+  pageParam: number;
+};
+
+type OsPaginatedResponse = {
+  orders: Os[];
+  totalPages: number;
+  page: number;
 };
 
 const useOrder = () => {
@@ -24,7 +25,7 @@ const useOrder = () => {
 
   const notify = useNotify();
 
-  const registerOrder = async (payload: OrderPayload) => {
+  const registerOrder = async (payload: Order) => {
     const urlRegisterOrder = `${BASE_URL}/order`;
     try {
       await axios.post(urlRegisterOrder, payload, header);
@@ -44,7 +45,45 @@ const useOrder = () => {
     }
   };
 
-  return {registerOrder};
+  const getAllOs = async ({
+    pageParam,
+  }: PageParam): Promise<OsPaginatedResponse | undefined> => {
+    const urlOs = `${BASE_URL}/order?page=${pageParam}`;
+    try {
+      const response = await axios.get(urlOs, header);
+      return response.data;
+    } catch (error) {
+      const err = error as AxiosError;
+      notify(
+        err.message as string,
+        notifyPositionMap.topRight,
+        notifyType.error
+      );
+    }
+  };
+
+  const {
+    data: paginatedOs,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+  } = useInfiniteQuery({
+    queryKey: ["getAllOs"],
+    queryFn: getAllOs,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.totalPages === lastPage?.page) return;
+      return Number(lastPage?.page) + 1;
+    },
+  });
+
+  const orders = paginatedOs
+    ? paginatedOs?.pages.flatMap((page) => {
+        return page?.orders;
+      })
+    : [];
+
+  return { registerOrder, fetchNextPage, hasNextPage, isLoading, orders };
 };
 
 export default useOrder;
