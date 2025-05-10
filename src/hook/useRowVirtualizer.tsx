550dd1939be0c2714b8c753ee39f3b85282d9ab2
Josuejcs13 <josuejcs13@gmail.com>
2025-05-10 15:51:22 -0400

    fix: remove unused import

--- src/hook/useRowVirtualizer.tsx
+++ src/hook/useRowVirtualizer.tsx
@@ -1,7 +1,7 @@
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { useEffect } from "react";
 import useAuthentication from "./useAuthentication";
-import useTechnician from "./useTechnician";
+
 import {
   FetchNextPageOptions,
   InfiniteData,
