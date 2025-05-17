a47dd8aadb8be6e4a372907e34389bff985dd172
Josieljcc <josiel.jcc@hotmail.com>
2025-05-17 16:27:33 -0300

    refactor: create a authenticated wrapper route

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -7,6 +7,7 @@ import Order from "@/Pages/order";
 import ListTechnician from "@/Pages/listTechnician";
 import ListClient from "@/Pages/listClient";
 import DetailClient from "@/Pages/detailClientPage";
+import AppLayout from "@/layout";
 
 const router = createBrowserRouter([
   {
@@ -14,37 +15,40 @@ const router = createBrowserRouter([
     element: <RegisterTechnician />,
   },
   {
-    path: "/home",
-    element: <Home />,
-  },
-  {
     path: "/login",
     element: <Login />,
   },
   {
-    path: "/register/technician",
-    element: <RegisterTechnician />,
+    path: "app",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "technician",
+        element: <ListTechnician />,
+      },
+      {
+        path: "client",
+        element: <ListClient />,
+      },
+      {
+        path: "order",
+        element: <Order />,
+      },
+      {
+        path: "technician",
+        element: <ListTechnician />,
+      },
+      {
+        path: "client/:id",
+        element: <DetailClient />,
+      },
+    ]
   },
-  {
-    path: "/technician",
-    element: <ListTechnician />,
-  },
-  {
-    path: "/client",
-    element: <ListClient />,
-  },
-  {
-    path: "/order",
-    element: <Order />,
-  },
-  {
-    path: "/technician",
-    element: <ListTechnician />,
-  },
-  {
-    path: "/client/:id",
-    element: <DetailClient />,
-  },
+
 ]);
 
 export default router;
