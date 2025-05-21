da2859a5194289aada94e74103ad4870a7c71fbf
Josuejcs13 <josuejcs13@gmail.com>
2025-05-21 19:30:53 -0400

    fix: change style for smaller screens and DrawerTrigger

--- src/components/formDrawer/formDrawer.tsx
+++ src/components/formDrawer/formDrawer.tsx
@@ -5,6 +5,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "../ui/drawer";
+import { IoAddOutline } from "react-icons/io5";
 
 type FormDrawerProps = {
   children: React.ReactNode;
@@ -16,10 +17,12 @@ type FormDrawerProps = {
 const FormDrawer = ({ children, buttonTitle, title = "" }: FormDrawerProps) => {
   return (
     <Drawer>
-      <DrawerTrigger className="text-white">{buttonTitle}</DrawerTrigger>
+      <DrawerTrigger className="text-mainColor px-4 pr-7 py-1 border-[.1563rem] text-sm items-center rounded-lg gap-2 font-medium flex   border-mainColor">
+        <IoAddOutline className="h-6 w-6" /> {buttonTitle}
+      </DrawerTrigger>
       <DrawerContent className="flex justify-center">
         <DrawerHeader className="flex justify-center flex-col items-center">
-          <DrawerTitle className="text-neutral-200 text-center text-4xl font-semibold pb-16">
+          <DrawerTitle className="text-neutral-200 text-center text-4xl font-semibold pb-10">
             {title}
           </DrawerTitle>
           {children}
