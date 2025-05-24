f4d54c2fae6a2801e68b168f2a1c5337d171e33f
Josuejcs13 <josuejcs13@gmail.com>
2025-05-24 15:31:57 -0400

    feat: add dynamic title and styling

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,19 +1,36 @@
-import { Outlet } from "react-router-dom"
-import SideBar from "./components/sideBar/sideBar"
-import useAuthentication from "./hook/useAuthentication"
+import { Outlet, useLocation } from "react-router-dom";
+import SideBar from "./components/sideBar/sideBar";
+import useAuthentication from "./hook/useAuthentication";
+import { TitleMapType } from "./types";
+
+const titleMap = {
+  "/app/client": "Clientes",
+  "/app/technician": "Técnicos",
+  "/app": "Home",
+};
 
 const AppLayout = () => {
-    useAuthentication()
+  useAuthentication();
 
-    return (
-        <div className="bg-zinc-800 text-zinc-100">
-            <header>header</header>
-            <div className="flex">
-                <SideBar />
-                <Outlet />
-            </div>
+  const { pathname } = useLocation();
+
+  if (!pathname) {
+    return;
+  }
+  return (
+    <div className="bg-zinc-800 text-zinc-100">
+      <header>header</header>
+      <div className="flex">
+        <SideBar />
+        <div className="w-full md:px-6 ">
+          <h1 className="font-semibold md:text-4xl text-2xl m-3">
+            {titleMap[pathname as TitleMapType]}
+          </h1>
+          <Outlet />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
