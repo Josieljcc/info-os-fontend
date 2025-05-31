f2cf4de78a56e5724d2c962042d5a4dda47ea7c3
Josuejcs13 <josuejcs13@gmail.com>
2025-05-31 14:04:32 -0400

    refactor: remove get functions and invalidate request when register a new client

--- src/hook/useClient/useClient.tsx
+++ src/hook/useClient/useClient.tsx
@@ -2,25 +2,14 @@ import { BASE_URL } from "@/constants";
 import UserContext from "@/context/userContext";
 import { editingClientType } from "@/schemas/editing";
 import { registerClientType } from "@/schemas/registerClient";
-import { Client, notifyPositionMap, notifyType, PageParam } from "@/types";
-import {
-  useQueryClient,
-  useMutation,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
+import { notifyPositionMap, notifyType } from "@/types";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
 import { useContext } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useNotify from "../useNotify";
 
-type ClientPaginatedResponse = {
-  clients: Client[];
-  totalPages: number;
-  page: number;
-};
-
 const useClient = () => {
-  const navigate = useNavigate();
   const notify = useNotify();
   const queryClient = useQueryClient();
   const { id } = useParams();
@@ -33,20 +22,16 @@ const useClient = () => {
 
   const registerClient = async (data: registerClientType) => {
     const urlRegisterClient = `${BASE_URL}/client`;
-
     try {
       await axios.post(urlRegisterClient, data, header);
-
+      queryClient.invalidateQueries({ queryKey: ["getAllClients"] });
       notify(
         "Cliente Registrado com Sucesso!",
         notifyPositionMap.topRight,
         notifyType.success
       );
-
-      navigate("/home");
     } catch (error) {
       const err = error as AxiosError;
-
       notify(
         err.message as string,
         notifyPositionMap.topRight,
@@ -81,61 +66,9 @@ const useClient = () => {
     },
   });
 
-  const getAllClients = async ({
-    pageParam,
-  }: PageParam): Promise<ClientPaginatedResponse | undefined> => {
-    const urlClient = `${BASE_URL}/client?page=${pageParam}&pageSize=${10}`;
-
-    try {
-      const response = await axios.get(urlClient, header);
-      return response.data;
-    } catch (error) {
-      const err = error as AxiosError;
-      notify(
-        err.message as string,
-        notifyPositionMap.topRight,
-        notifyType.error
-      );
-    }
-  };
-
-  const {
-    data: paginatedClients,
-    fetchNextPage,
-    hasNextPage,
-    isLoading,
-    isFetchingNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["getAllClients"],
-    queryFn: getAllClients,
-    initialPageParam: 1,
-    getNextPageParam: (lastPage) => {
-      if (lastPage?.totalPages === lastPage?.page) return;
-      return Number(lastPage?.page) + 1;
-    },
-  });
-
-  const clients = paginatedClients
-    ? paginatedClients?.pages.flatMap((page) => {
-        return page?.clients;
-      })
-    : [];
-
-  const getClientById = async (id: string): Promise<Client> => {
-    const urlClientById = `${BASE_URL}/client/${id}`;
-    const response = await axios.get(urlClientById, header);
-    return response.data;
-  };
-
   return {
     registerClient,
-    isLoading,
-    clients,
-    fetchNextPage,
-    hasNextPage,
-    getClientById,
     editClientMutation,
-    isFetchingNextPage,
   };
 };
 
