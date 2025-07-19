3eb39192c30e69590fc0c77157fef55b747a3abd
Josuejcs13 <josuejcs13@gmail.com>
2025-07-19 15:12:02 -0400

    feat: add style equipment in dashboard

--- src/components/sideBar/constants.tsx
+++ src/components/sideBar/constants.tsx
@@ -3,6 +3,7 @@ import { LiaHomeSolid } from "react-icons/lia";
 import { FiUser } from "react-icons/fi";
 import { VscTools } from "react-icons/vsc";
 import { HiMiniComputerDesktop } from "react-icons/hi2";
+import { PiToolboxLight } from "react-icons/pi";
 
 export const menuItems: SideBarItemType[] = [
   {
@@ -24,5 +25,10 @@ export const menuItems: SideBarItemType[] = [
     path: "/app/part",
     icon: HiMiniComputerDesktop,
     title: "Peças",
+  },
+  {
+    path: "/app/service",
+    icon: PiToolboxLight,
+    title: "Serviços",
   },
 ];
