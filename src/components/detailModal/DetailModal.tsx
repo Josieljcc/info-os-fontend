930d638aae11ced8ecf4a0707de5144e9799cf14
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-07-26 15:09:29 -0300

    style: reformat DetailModal component for consistency

--- src/components/detailModal/DetailModal.tsx
+++ src/components/detailModal/DetailModal.tsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
-import { AlertDialog, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 import { Client, Technician } from "@/types";
 import DetailUser from "@/Pages/detailClientPage";
 
 type detailModalProps = {
-    icon?: React.ReactNode
-    user?: Client | Technician
-}
+  icon?: React.ReactNode;
+  user?: Client | Technician;
+};
 
 const DetailModal = ({ icon, user }: detailModalProps) => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <AlertDialog open={open} onOpenChange={setOpen}>
-            <AlertDialogTrigger className="bg-mainColor p-2 rounded-lg">{icon}</AlertDialogTrigger>
-            <AlertDialogContent className="bg-secondaryColor text-gray-100">
-                <AlertDialogTitle className="text-white">Detalhes</AlertDialogTitle>
-                <AlertDialogHeader>
-                    <DetailUser
-                        user={user as Client} />
-                </AlertDialogHeader>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
-}
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger className="bg-mainColor p-2 rounded-lg">
+        {icon}
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-secondaryColor text-gray-100">
+        <AlertDialogTitle className="text-white">Detalhes</AlertDialogTitle>
+        <AlertDialogHeader>
+          <DetailUser user={user as Client} />
+        </AlertDialogHeader>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
-export default DetailModal
+export default DetailModal;
