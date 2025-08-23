76f0987c316595180e600534bdc148a54046dd9a
lucas-de-lima <lucasramosdelima@gmail.com>
2025-08-22 21:25:07 -0300

    chore: update setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,7 @@
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
+import '@testing-library/jest-dom';
 
 afterEach(() => {
-    cleanup();
+  cleanup();
 });
