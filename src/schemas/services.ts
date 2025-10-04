1185ecafc3e6ba770dcdbdcec40d9aed7e6fa51d
Josiel da Costa Carvaho <josiel.jcc@hotmail.com>
2025-10-04 14:48:25 -0300

    Revert "Revert "Fix Os register""

--- src/schemas/services.ts
+++ src/schemas/services.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export type ServicesType = z.infer<typeof servicesSchema>;
 
 const servicesSchema = z.object({
+  id: z.number().optional(),
   name: z.string().min(1, "Nome é obrigatório"),
   description: z.string(),
   price: z.coerce.number().min(1, "Preço é obrigatório"),
