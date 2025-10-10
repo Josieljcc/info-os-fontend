ea3bb790dfc2d8931f54febe1ebf6a8130000399
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-10-10 16:12:46 -0300

    feat: update sidebar menu items with new icons and add O.S. path

--- src/components/sideBar/constants.tsx
+++ src/components/sideBar/constants.tsx
@@ -3,7 +3,9 @@ import { LiaHomeSolid } from "react-icons/lia";
 import { FiUser } from "react-icons/fi";
 import { VscTools } from "react-icons/vsc";
 import { HiMiniComputerDesktop } from "react-icons/hi2";
+import { BsGear } from "react-icons/bs";
 import { PiToolboxLight } from "react-icons/pi";
+import { HiOutlineDocumentText } from "react-icons/hi2";
 
 export const menuItems: SideBarItemType[] = [
   {
@@ -23,18 +25,22 @@ export const menuItems: SideBarItemType[] = [
   },
   {
     path: "/app/part",
-    icon: HiMiniComputerDesktop,
+    icon: BsGear,
     title: "Peças",
   },
   {
     path: "/app/service",
     icon: PiToolboxLight,
     title: "Serviços",
-
   },
   {
     path: "/app/equipment",
     icon: HiMiniComputerDesktop,
     title: "Equipamento",
+  },
+  {
+    path: "/app/os",
+    icon: HiOutlineDocumentText,
+    title: "O.S.",
   },
 ];
