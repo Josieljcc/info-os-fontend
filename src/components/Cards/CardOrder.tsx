8437bbdbb2c71b16a04afe7824539f2571c5fffb
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-11-08 14:36:55 -0300

    feat: refactor CardOrderService for improved readability and status display

--- src/components/Cards/CardOrder.tsx
+++ src/components/Cards/CardOrder.tsx
@@ -1,30 +1,52 @@
 import { OrderResponse } from "@/types";
-import { formatDateBR } from "@/lib/utils";
 import DetailModalOrderService from "../detailModal/orderServiceModal";
 import DeleteButton from "../deleteButton/deleteButton";
 import useDeleteOrderService from "@/hook/useOrderService/useDeleteOrderService";
+import { format } from "date-fns";
+import { items } from "@/components/selectStatusOrder/constants";
+import { cn } from "@/lib/utils";
 
 type CardOrderServiceProps = { classname?: string; order: OrderResponse };
 
 const CardOrderService = ({ order, classname }: CardOrderServiceProps) => {
   const { deleteOrderService } = useDeleteOrderService();
+
   const handleDelete = async () => {
     await deleteOrderService(order.id);
   };
+
+  const currentStatus = items.find((item) => item.value === order.status);
+
   return (
     <div
-      className={`flex justify-between h-16 text-white rounded-2xl border-2 items-center w-full border-[#e9ecef7b] md:py-5 py-3 md:px-7 px-4 bg-secondaryColor ${classname}`}
+      className={cn(
+        "flex justify-between gap-4 h-16 text-white rounded-2xl border-2 items-center w-full border-[#e9ecef7b] md:py-5 py-3 md:px-7 px-4 bg-secondaryColor",
+        classname
+      )}
     >
-      <div className="flex flex-1 items-center">
-        <p className="text-sm font-medium md:w-1/3 w-1/2">#{order?.id}</p>
-        <p className="text-sm font-medium w-1/3 pl-6">{order?.client?.name}</p>
-        <p className="text-sm font-medium w-1/3 pl-6">
-          {formatDateBR(order?.openingDate)}
-        </p>
-        <p className="text-sm font-medium w-1/3 pl-6">
-          {formatDateBR(order?.forecastDate)}
-        </p>
+      <p className="text-sm font-medium">#{order?.id}</p>
+      <p className="text-sm font-medium flex-1 truncate">
+        {order?.client?.name}
+      </p>
+      <p className="text-sm font-medium hidden sm:block">
+        {format(order?.openingDate, "dd-MM-yyyy")}
+      </p>
+      <p className="text-sm font-medium hidden sm:block">
+        {format(order?.forecastDate, "dd-MM-yyyy")}
+      </p>
+
+      <div className="flex justify-start items-center gap-1 sm:min-w-[100.5px]">
+        <div
+          className={cn(
+            "w-3 h-3 rounded-full",
+            currentStatus?.color || "bg-gray-500"
+          )}
+        />
+        <span className="text-sm text-gray-300 capitalize hidden sm:block">
+          {currentStatus?.label || "sem status"}
+        </span>
       </div>
+
       <div className="flex gap-2 h-7 items-center">
         <DetailModalOrderService order={order} />
         <DeleteButton
@@ -36,4 +58,5 @@ const CardOrderService = ({ order, classname }: CardOrderServiceProps) => {
     </div>
   );
 };
+
 export default CardOrderService;
