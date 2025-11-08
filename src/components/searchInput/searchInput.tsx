bba6414fd25ed77a0c2c6c07e2a9fa4d8d0115fd
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-11-08 14:37:53 -0300

    feat: make placeholder prop configurable in SearchInput component

--- src/components/searchInput/searchInput.tsx
+++ src/components/searchInput/searchInput.tsx
@@ -3,16 +3,17 @@ import { FiSearch } from "react-icons/fi";
 
 type SearchInputProp = {
   setValue: (value: React.SetStateAction<string>) => void;
+  placeholder?: string;
 };
 
-const SearchInput = ({ setValue }: SearchInputProp) => {
+const SearchInput = ({ setValue, placeholder = "Search" }: SearchInputProp) => {
   return (
     <div className="bg-[#3f3f3f] flex items-center gap-2 w-full p-2 text-[#656565] rounded-lg">
       <FiSearch />
       <input
         className="bg-transparent h-full focus:outline-none w-full text-white placeholder:text-[#656565]"
         onChange={(event) => setValue(event.target.value)}
-        placeholder="Search"
+        placeholder={placeholder}
       />
       <BsSlashSquareFill className="text-secondaryColor" />
     </div>
