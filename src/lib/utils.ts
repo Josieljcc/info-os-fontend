64eedcfa800c80eed11db264e55670ad4bad3270
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-11-08 14:39:16 -0300

    refactor: remove unused formatDateBR function from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,14 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatDateBR(dateString?: string | null): string {
-  if (!dateString) return "";
-  const date = new Date(dateString);
-  if (isNaN(date.getTime())) return dateString;
-  return new Intl.DateTimeFormat("pt-BR", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  }).format(date);
-}
