e6b883fa22e77fdd1b5641db21111a5e018e3ea3
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2025-11-14 20:53:33 -0300

    feat: add getForecastParams function to handle forecast date logic

--- src/util/dateUtil.ts
+++ src/util/dateUtil.ts
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format, isSameDay } from "date-fns";
 
 const formatToSave = (date: string) => {
   return format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
@@ -8,4 +8,21 @@ const formatToShow = (date: string | Date) => {
   return format(date, "dd/MM/yyyy");
 };
 
-export { formatToSave, formatToShow };
+type ForecastParams = {
+  forecastdate?: string;
+  forecastStartDate?: string;
+  forecastEndDate?: string;
+};
+
+const getForecastParams = (
+  startDate?: string,
+  endDate?: string
+): ForecastParams => {
+  if (startDate && endDate && isSameDay(new Date(startDate), new Date(endDate))) {
+    return { forecastdate: startDate };
+  } else {
+    return { forecastStartDate: startDate, forecastEndDate: endDate };
+  }
+};
+
+export { formatToSave, formatToShow, getForecastParams };
